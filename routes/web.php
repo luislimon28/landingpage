65c4feab7cbadca71bbd4ecca3afb213ad7cd4af
luislimon28 <luis.limon4387@alumnos.udg.mx>
2022-11-12 05:13:56 -0600

    Primer commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('landingpage', function () {
+    return view('landingpage');
+});
+
+Route::get('contacto', function () {
+    return view('contacto');
+});
